7fcc4025f6b3c3cd10c43690e3264367339c1321
Polikkox <damian.targosz11@gmail.com>
2019-01-29 11:34:22 +0100

    Add pop method and test for it Add peek method

--- src/main/java/Stack.java
+++ src/main/java/Stack.java
@@ -1,13 +1,25 @@
 public class Stack<E> {
 
     private E[] data;
+    private int len;
 
     @SuppressWarnings({"unchecked"})
     public Stack(){
         this.data = (E[]) new Object[500];
+        this.len = 0;
     }
 
-    public void push(){
+    public void push(E element){
+        data[len++] = element;
+    }
 
+    public E pop(){
+        E element = peek();
+        this.data[--len] = null;
+        return element;
+    }
+
+    public E peek(){
+        return this.data[len - 1];
     }
 }
--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -1,13 +1,36 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StackTest {
 
+    private Stack<String> stack;
+
+    @BeforeEach
+    public void createInstanceOfClass(){
+        this.stack = new Stack<>();
+    }
+
     @Test
     public void testCreateInstanceOfClassCorrectly(){
-        Stack<String> stack = new Stack<>();
         assertNotNull(stack);
     }
+
+    @Test
+    public void testAddElementToList(){
+        stack.push("test1");
+    }
+
+    @Test void  testPopReturnCorrectElement(){
+        String expected = "test1";
+        String expected2 = "test2";
+        stack.push(expected);
+        stack.push(expected2);
+        assertEquals(expected2, stack.pop());
+    }
+
+
+
 
 }
